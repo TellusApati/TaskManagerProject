c8c3df7d88393eb4413e1d5395df8712ca3016ed
TellusApati <135984644+TellusApati@users.noreply.github.com>
2025-11-17 00:52:19 +0300

    Update from previous seminar

--- code.js
+++ code.js
@@ -1,34 +1,67 @@
 
-const buttonAdd = document.querySelector("#todo .buttonAdd");
+const buttonAddToDo = document.querySelector("#todo .buttonAdd");
+const buttonAddInProgress = document.querySelector("#inprogress .buttonAdd");
+const buttonAddDone = document.querySelector("#done .buttonAdd");
 
-const todoTasks = document.querySelector("#todo .tasks");
+const tasksToDoObject = document.querySelector("#todo .tasks");
+const tasksInProgressObject = document.querySelector("#inprogress .tasks");
+const tasksDoneObject = document.querySelector("#done .tasks");
 
 
 
-
-var tasks = [
+var tasksToDo = [
     {
         "name": "Testing task",
         "isDone": false
     }
 ]
 
+var tasksInProgress = []
+var tasksDone = []
 
-buttonAdd.addEventListener("click", () => {
+if (localStorage.getItem("tasksToDo") != null) {
+    tasksToDo = localStorage.getItem("tasksToDo");
+}
+if (localStorage.getItem("tasksInProgress") != null) {
+    tasksInProgress = localStorage.getItem("tasksInProgress");
+}
+if (localStorage.getItem("tasksDone") != null) {
+    tasksDone = localStorage.getItem("tasksDone");
+}
 
-    tasks.push(
+buttonAddToDo.addEventListener("click", () => {
+    tasksToDo.push(
         {
             "name":"",
             "isDone": false
         }
     );
-    updateTasks();
+updateTasksAll();
+})
+buttonAddInProgress.addEventListener("click", () => {
+    tasksInProgress.push(
+        {
+            "name":"",
+            "isDone": false
+        }
+    );
+updateTasksAll();
+})
+buttonAddDone.addEventListener("click", () => {
+    tasksDone.push(
+        {
+            "name":"",
+            "isDone": false
+        }
+    );
+updateTasksAll();
 })
 
-const updateTasks = () => {
-    let toRemove = todoTasks.querySelectorAll(".task");
+
+const updateTasks = (tasks, tasksObject, buttonAdd) => {
+    let toRemove = tasksObject.querySelectorAll(".task");
     toRemove.forEach(element => {
-        todoTasks.removeChild(element);
+        tasksObject.removeChild(element);
     });
     for (let i = 0; i < tasks.length; i ++) {
         let task = document.createElement("div");
@@ -49,14 +82,17 @@ const updateTasks = () => {
                 task.classList.add("done");
             }
             tasks[i].isDone = !tasks[i].isDone;
+            saveAll();
             
         });
 
         let textArea = document.createElement("textarea");
         textArea.textContent = tasks[i].name;
         textArea.classList.add("name");
+        textArea.maxLength = 40
         textArea.addEventListener("input", () => {
             tasks[i].name = textArea.value;
+            saveAll();
         });
 
         let buttonRemove = document.createElement("button");
@@ -67,6 +103,7 @@ const updateTasks = () => {
         buttonRemove.addEventListener("click", () => {
             tasks.splice(i, 1);
             updateTasks();
+            saveAll();
         });
 
         task.append(buttonDone);
@@ -74,8 +111,20 @@ const updateTasks = () => {
         task.append(buttonRemove);
 
 
-        todoTasks.insertBefore(task, buttonAdd);
+        tasksObject.insertBefore(task, buttonAdd);
     }
 }
 
-updateTasks();
+const updateTasksAll = () => {
+    updateTasks(tasksToDo, tasksToDoObject, buttonAddToDo);
+    updateTasks(tasksInProgress, tasksInProgressObject, buttonAddInProgress);
+    updateTasks(tasksDone, tasksDoneObject, buttonAddDone);
+}
+
+const saveAll = () => {
+    localStorage.setItem("tasksToDo", JSON.stringify(tasksToDo));
+    localStorage.setItem("tasksInProgress", JSON.stringify(tasksInProgress));
+    localStorage.setItem("tasksDone", JSON.stringify(tasksDone));
+}
+
+updateTasksAll();
